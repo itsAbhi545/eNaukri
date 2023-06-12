ee5b233013d588d4867a94333b6e112cb487daf6
Harman <harmanjyot.singh@chicmic.co.in>
2023-06-12 16:50:57 +0530

    Created and change user profile

--- src/main/java/com/chicmic/eNaukri/model/UserProfile.java
+++ src/main/java/com/chicmic/eNaukri/model/UserProfile.java
@@ -1,5 +1,51 @@
 package com.chicmic.eNaukri.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//@TrimAll
+@Entity
+@RequiredArgsConstructor
+@Getter
+@Setter
 public class UserProfile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String currentCompany;
+    private String cvPath;
+    private String bio;
+    private String ppPath;
+
+
+    private boolean hasPremium;
+    @OneToOne(mappedBy = "userSubscription", cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+    @JsonIgnore
+    @JsonIgnoreProperties({"handler", "hibernateLazyInitializer"})
+    private Premium premium;
+    @OneToOne(mappedBy = "userPreferences", cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+    @JsonIgnore
+    @JsonIgnoreProperties({"handler", "hibernateLazyInitializer"})
+    private Preference preference;
+
+    @OneToMany(mappedBy = "edUser", cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+    @JsonIgnoreProperties({"handler", "hibernateLazyInitializer"})
+    private List<Education> educationList=new ArrayList<>();
+
+    @OneToMany(mappedBy = "applicantId", cascade = CascadeType.ALL,fetch = FetchType.EAGER)
+    private List<Application> applicationList=new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL,fetch =FetchType.EAGER)
+    private List<UserSkills> userSkillsList=new ArrayList<>();
+    @OneToMany(mappedBy = "expUser",orphanRemoval = true, cascade = CascadeType.ALL,fetch = FetchType.EAGER)
+    private List<Experience> experienceList=new ArrayList<>();
+
 
 }
--- src/main/java/com/chicmic/eNaukri/model/Users.java
+++ src/main/java/com/chicmic/eNaukri/model/Users.java
@@ -43,50 +43,24 @@ public class Users {
     @Pattern(regexp = RegEx.PASSWORD,message = "")
     @NotNull
     private String password;
+    private String otp;
+    private boolean isVerified;
+    @UuidGenerator
+    private String uuid;
     @Pattern(regexp = RegEx.PHONENUMBER,message = "")
     @NotNull
     private String phoneNumber;
-    private String currentCompany;
-    private String cvPath;
-    private String bio;
-    private String ppPath;
-    private String otp;
-    @UuidGenerator
-    private String uuid;
-    private String link;
-    private boolean isVerified;
     private boolean enableNotification;
-    private boolean hasPremium;
 
-//mappings
+    //mappings
 
     @OneToOne(mappedBy = "userLinks", cascade = CascadeType.ALL,fetch = FetchType.LAZY)
     @JsonIgnore
     @JsonIgnoreProperties({"handler", "hibernateLazyInitializer"})
     private SocialLink socialLink;
-    @OneToOne(mappedBy = "userSubscription", cascade = CascadeType.ALL,fetch = FetchType.LAZY)
-    @JsonIgnore
-    @JsonIgnoreProperties({"handler", "hibernateLazyInitializer"})
-    private Premium premium;
-    @OneToOne(mappedBy = "userPreferences", cascade = CascadeType.ALL,fetch = FetchType.LAZY)
-    @JsonIgnore
-    @JsonIgnoreProperties({"handler", "hibernateLazyInitializer"})
-    private Preference preference;
-
-    @OneToMany(mappedBy = "edUser", cascade = CascadeType.ALL,fetch = FetchType.LAZY)
-    @JsonIgnoreProperties({"handler", "hibernateLazyInitializer"})
-    private List<Education> educationList=new ArrayList<>();
-    boolean success =Hibernate.isInitialized(educationList);
-    @OneToMany(mappedBy = "applicantId", cascade = CascadeType.ALL,fetch = FetchType.EAGER)
-    private List<Application> applicationList=new ArrayList<>();
-
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL,fetch =FetchType.EAGER)
-    private List<UserSkills> userSkillsList=new ArrayList<>();
 
     @OneToMany(cascade = CascadeType.ALL,fetch=FetchType.EAGER,mappedBy = "userr")
     @JsonIgnore
     private Set<UserToken> userTokenSet=new HashSet<>();
 
-    @OneToMany(mappedBy = "expUser",orphanRemoval = true, cascade = CascadeType.ALL,fetch = FetchType.EAGER)
-    private List<Experience> experienceList=new ArrayList<>();
 }
